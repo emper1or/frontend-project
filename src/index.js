8aeeebe0a76b24eacbcf5243aa912ce0f25405c0
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-10-29 00:17:39 +0700

    feat: adaptive disign

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './index.css';
 
 const menu = document.querySelector('.mobile-menu__inner');
+const openMenuButton = document.querySelector('.js-menu-active');
+const closeMenuButton = document.querySelector('.js-menu-close');
 
-document.querySelector('.js-menu-active').addEventListener('click', () => {
+openMenuButton.addEventListener('click', () => {
   menu.classList.toggle('mobile-menu__inner--active');
+});
+
+closeMenuButton.addEventListener('click', () => {
+  menu.classList.remove('mobile-menu__inner--active');
+});
+
+// Close menu when clicking outside
+document.addEventListener('click', (event) => {
+  if (!menu.contains(event.target) && !openMenuButton.contains(event.target) && menu.classList.contains('mobile-menu__inner--active')) {
+    menu.classList.remove('mobile-menu__inner--active');
+  }
 });
